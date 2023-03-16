9514a4ef013c789db1f2eaccf44190975f1fdac0
jordancuadrado <jac59155@gmail.com>
2023-03-16 19:14:48 -0400

    Update azure

---
+++ azuredb.js
@@ -0,0 +1,114 @@
+const sql = require('mssql');
+const express = require('express');
+const app = express();
+
+const config = {
+  server: 'widgetdataserver.database.windows.net',
+  database: 'LargeDataTest',
+  user: 'widgetadmin@widgetdataserver',
+  password: 'SolidWorks1',
+  encrypt: true
+}
+
+const pool = new sql.ConnectionPool(config);
+
+
+app.get('/atms', (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  pool.connect(err => {
+    if (err) {
+      console.error('Error connecting to Azure SQL database', err)
+    } else {
+      const request = new sql.Request(pool)
+      request.query('SELECT * FROM dbo.widget', (err, result) => {
+        if (err) {
+          console.error('Error executing query', err)
+        } else {
+  
+          const output = {
+            meta: {
+              updated: new Date().toISOString()
+            },
+            data: result.recordset.map(record => ({
+              id: record.id,
+              type: record.type,
+              Access: record.Access,
+              DisplayOnWeb: record.DisplayOnWeb,
+              attributes: {
+                creditUnion: record.attributescreditUnion,
+                transactionTypes: record.attributestransactionTypes,
+                name: record.attributesname,
+                address1: record.attributesaddress1,
+                City: record.attributesCity,
+                State: record.attributesState,
+                zipcode: record.attributeszipcode,
+                locationDescription: record.attributeslocationDescription,
+                latitude: record.attributeslatitude,
+                longitude: record.attributeslongitude
+              }
+            }))
+          }
+
+          res.json(output);
+          // console.log(output)
+        }
+      })
+    }
+  })
+
+})
+
+app.get('/atms/coop', (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  pool.connect(err => {
+    if (err) {
+      console.error('Error connecting to Azure SQL database', err)
+    } else {
+      const request = new sql.Request(pool)
+      request.query('SELECT * FROM dbo.widget WHERE attributescreditUnion is not NULL;', (err, result) => {
+        if (err) {
+          console.error('Error executing query', err)
+        } else {
+  
+          const output = {
+            meta: {
+              updated: new Date().toISOString()
+            },
+            data: result.recordset.map(record => ({
+              id: record.id,
+              type: record.type,
+              Access: record.Access,
+              DisplayOnWeb: record.DisplayOnWeb,
+              attributes: {
+                creditUnion: record.attributescreditUnion,
+                transactionTypes: record.attributestransactionTypes,
+                name: record.attributesname,
+                address1: record.attributesaddress1,
+                City: record.attributesCity,
+                State: record.attributesState,
+                zipcode: record.attributeszipcode,
+                locationDescription: record.attributeslocationDescription,
+                latitude: record.attributeslatitude,
+                longitude: record.attributeslongitude
+              }
+            }))
+          }
+
+          res.json(output);
+          // console.log(output)
+        }
+      })
+    }
+  })
+
+})
+
+
+
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
